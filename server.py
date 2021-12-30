1069b6d4927529f946762fbe4440865bd8f7203f
Kathryn Wint <wintkathryn@gmail.com>
2021-12-29 22:39:59 -0800

    Images for fruits were added, data from form now reflected on checkout page with the corr ect date and time

--- server.py
+++ server.py
@@ -5,10 +5,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['GET','POST'])         
 def checkout():
+    import datetime
+    now = datetime.datetime.now()
+    date =now.strftime("%m-%d-%Y- %H:%M:%S")
     print(request.form)
-    return render_template("checkout.html")
+    data = {
+        'strawberry':request.form['strawberry'],
+        'raspberry': request.form['raspberry'],
+        'apple':request.form['apple'],
+        'blackberry':request.form['blackberry'],
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'student_id':request.form['student_id']
+    }
+    print("Charging {{first_name}} {{last_name}} for (count) fruits.")
+    return render_template("checkout.html", date = date, data = data)
 
 @app.route('/fruits')         
 def fruits():
